dc27f8e12e35a29880409202587541a363e75615
mridul.shukla2 <mridul.shukla@vecmocon.com>
2026-01-03 17:31:01 +0530

    configured aceternity ui and built nav+hero section

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/ui/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/96 antialiased bg-grid-white/2">
+      <h1 className="text-2xl text-center">Chai aur code</h1>
+      <HeroSection />
+    </main>
+  );
+}
