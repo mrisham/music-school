f69235345e6bbfc823ac58943804f403af1d7f15
mridul.shukla2 <mridul.shukla@vecmocon.com>
2026-01-03 18:33:58 +0530

    added featured section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import FeaturedCourses from "@/components/ui/FeaturedCourses";
 import HeroSection from "@/components/ui/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased bg-grid-white/2">
-      <h1 className="text-2xl text-center">Chai aur code</h1>
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
